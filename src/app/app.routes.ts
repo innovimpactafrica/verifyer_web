042022f64d6136ee42458d324b45f98aa38434d3
Cheikh-ibraa <sidieng@innovimpactafrica.com>
2025-10-28 11:24:26 +0000

    implémentaion de la page détail certification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,8 +43,12 @@ export const routes: Routes = [
         path: 'detail-paiement/:id',
         loadComponent: () => import('./components/client/demandes/detail-paiement/detail-paiement.component').then(m => m.DetailPaiementComponent)
     },
-    {    
+    {
         path: 'mes-certifications',
         loadComponent: () => import('./components/client/certifications/mes-certifications/mes-certifications.component').then(m => m.MesCertificationsComponent)
+    },
+    {
+        path: 'detail-certification/:id',
+        loadComponent: () => import('./components/client/certifications/detail-certification/detail-certification.component').then(m => m.DetailCertificationComponent)
     },
 ];
